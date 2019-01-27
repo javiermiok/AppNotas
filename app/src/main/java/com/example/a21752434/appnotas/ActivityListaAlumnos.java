29d158e1c75647030ee7cf1b95d1784551a8b49a
Javier <javiermiok@gmail.com>
2019-01-27 15:12:33 +0100

    Tercer Commit Añadido atributo y parámetro de Contexto en la clase Adaptador para acceder a los recursos String.

--- app/src/main/java/com/example/a21752434/appnotas/ActivityListaAlumnos.java
+++ app/src/main/java/com/example/a21752434/appnotas/ActivityListaAlumnos.java
@@ -39,7 +39,7 @@ public class ActivityListaAlumnos extends AppCompatActivity {
 
         //TODO esta opción es mejor?
         listaResultado = getIntent().<AlumnoAsig>getParcelableArrayListExtra("LISTA_RESULTADO");
-        adapterN = new AdaptadorNotas(listaResultado);
+        adapterN = new AdaptadorNotas(listaResultado, getApplicationContext());
         adapterN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/a21752434/appnotas/AdaptadorNotas.java
+++ app/src/main/java/com/example/a21752434/appnotas/AdaptadorNotas.java
@@ -1,5 +1,6 @@
 package com.example.a21752434.appnotas;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -17,10 +18,12 @@ public class AdaptadorNotas extends RecyclerView.Adapter<AdaptadorNotas.NotasVie
     private ArrayList<AlumnoAsig> datos;
     //Nuevo
     private View.OnClickListener listener;
+    private Context context;
 
     //TODO Añadir contexto como parámetro
-    public AdaptadorNotas(ArrayList<AlumnoAsig> datos) {
+    public AdaptadorNotas(ArrayList<AlumnoAsig> datos, Context context) {
         this.datos = datos;
+        this.context = context;
     }
 
     @NonNull
@@ -28,7 +31,7 @@ public class AdaptadorNotas extends RecyclerView.Adapter<AdaptadorNotas.NotasVie
     public NotasViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.alumno_item, viewGroup,false);
         v.setOnClickListener(this);
-        NotasViewHolder nvh = new NotasViewHolder(v);
+        NotasViewHolder nvh = new NotasViewHolder(v,context);
         return nvh;
     }
 
@@ -60,9 +63,11 @@ public class AdaptadorNotas extends RecyclerView.Adapter<AdaptadorNotas.NotasVie
         private TextView tvNotaExamen;
         private TextView tvNotaTareas;
         private TextView tvNotaActitud;
+        private Context context;
 
-        public NotasViewHolder(@NonNull View itemView) {
+        public NotasViewHolder(@NonNull View itemView, Context context) {
             super(itemView);
+            this.context = context;
             tvNombre = itemView.findViewById(R.id.tvNombreAl);
             tvNotaExamen = itemView.findViewById(R.id.tvNotaExamen);
             tvNotaTareas = itemView.findViewById(R.id.tvNotaTareas);
@@ -72,9 +77,9 @@ public class AdaptadorNotas extends RecyclerView.Adapter<AdaptadorNotas.NotasVie
         //TODO getString(String.format());...
         public void bindPatron(AlumnoAsig alum) {
             tvNombre.setText(alum.getNombre());
-            tvNotaExamen.setText(String.valueOf(alum.getNotaExamen()));
-            tvNotaTareas.setText(String.valueOf(alum.getNotaTarea()));
-            tvNotaActitud.setText(String.valueOf(alum.getNotaActitud()));
+            tvNotaExamen.setText(String.format(context.getString(R.string.tv_nota_examen),alum.getNotaExamen()));
+            tvNotaTareas.setText(String.format(context.getString(R.string.tv_nota_tareas),alum.getNotaTarea()));
+            tvNotaActitud.setText(String.format(context.getString(R.string.tv_nota_actitud),alum.getNotaActitud()));
         }
     }
 
